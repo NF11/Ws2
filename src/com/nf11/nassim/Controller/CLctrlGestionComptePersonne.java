e45b69c6f2d17989f94885c3d6e6f056a3a0ed5e
NF11 <nassimfares2019@outlook.fr>
2019-09-24 09:54:43 +0100

    controller

---
+++ src/com/nf11/nassim/Controller/CLctrlGestionComptePersonne.java
@@ -0,0 +1,9 @@
+package com.nf11.nassim.Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CLctrlGestionComptePersonne {
+}
--- src/com/nf11/nassim/Model/CLctrlGestionComptePersonne.java
+++
@@ -1,4 +0,0 @@
-package com.nf11.nassim.Model;
-
-public class CLctrlGestionComptePersonne {
-}
