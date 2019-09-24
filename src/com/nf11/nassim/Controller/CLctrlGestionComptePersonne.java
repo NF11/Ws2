97814a6b4dcbcd9373b4e11b4c468d9929fe3e06
Ordana <Ordana1234@gmail.com>
2019-09-24 10:58:42 +0100

    addec controller changes

--- src/com/nf11/nassim/Controller/CLctrlGestionComptePersonne.java
+++ src/com/nf11/nassim/Controller/CLctrlGestionComptePersonne.java
@@ -1,4 +1,32 @@
 package com.nf11.nassim.Controller;
+import com.nf11.nassim.Model.CLcad;
+import com.nf11.nassim.Model.CLmapTB_PERSONNE;
 
+import java.sql.*;
 public class CLctrlGestionComptePersonne {
+    CLcad conn;
+    CLmapTB_PERSONNE personne;
+    public CLctrlGestionComptePersonne{
+        try {
+            this.conn = new CLcad();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        this.personne = new CLmapTB_PERSONNE();
+    }
+    public ResultSet m_listerLesCompte() throws SQLException, ClassNotFoundException {
+
+
+        return this.conn.m_getRows(personne.m_select());
+    }
+
+    public int m_CreeUnCompte(int id, String name, String firstName) throws SQLException {
+
+        return  this.conn.m_actionRows(this.personne.m_insert(id,name,firstName));
+    }
+
+
 }
--- src/com/nf11/nassim/Model/CLmapTB_PERSONNE.java
+++ src/com/nf11/nassim/Model/CLmapTB_PERSONNE.java
@@ -1,4 +1,55 @@
 package com.nf11.nassim.Model;
 
 public class CLmapTB_PERSONNE {
+
+    private int id;
+    private String name;
+    private String firstName;
+    private String query;
+
+    public String m_select(){
+        return "SELECT * from tb_personne;";
+    }
+
+    public String m_insert(int id, String name, String firstName){
+        return "INSERT INTO tb_personne (id,name,firstName) VALUES ("+id+
+                "," + name +
+                "," + firstName+
+                ")";
+    }
+
+
+    // Getters & Setters
+    //------------------------------------------------------
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
 }
